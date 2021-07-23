b8897e9e200f146b1cc7aaf67e83ed378ca830f5
GuAugusto <gustavoaugusto77@hotmail.com>
2021-07-23 15:21:24 -0300

    sexta

---
+++ hello/src/main/java/com/helloworld/hello/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.helloworld.hello.controller;
+
+import java.security.PublicKey;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+	@GetMapping
+	public String hello () {
+		return "Olá Generation, tive que ter muita persistencia para conseguir entender a nova materia.";
+	}
+}
