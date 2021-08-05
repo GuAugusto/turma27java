e2c159ea3b3004b6a07e4432fdff76d6f652a544
GuAugusto <gustavoaugusto77@hotmail.com>
2021-08-05 15:05:10 -0300

    Atividade security Back-end blog-pessoal

--- lojagames/src/main/java/com/generation/lojagames/Controller/CategoriaController.java
+++ lojagames/src/main/java/com/generation/lojagames/Controller/CategoriaController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.lojagames.model.Categoria;
 
-
 @RestController
 @RequestMapping("/categoria")
 @CrossOrigin("*")
@@ -25,48 +24,40 @@ public class CategoriaController {
 
 	@Autowired
 	private com.generation.lojagames.Repository.CategoriaRepository repository;
-	
+
 	@GetMapping
-	public ResponseEntity<List<Categoria>> getAll(){
+	public ResponseEntity<List<Categoria>> getAll() {
 		return ResponseEntity.ok(repository.findAll());
 	}
-	
+
 	@GetMapping("/{id}")
-	public ResponseEntity<Categoria> getById(@PathVariable long id){
-		
+	public ResponseEntity<Categoria> getById(@PathVariable long id) {
+
 		return repository.findById(id).map(resp -> ResponseEntity.ok(resp))
-				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());		
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 	}
-	
+
 	@GetMapping("/descricao/{descricao}")
-	public ResponseEntity<List<Categoria>> getAllDescricao(@PathVariable String descricao){
-		
-		return ResponseEntity.ok(repository.findAllByDescricaoContainingIgnoreCase(descricao));	
+	public ResponseEntity<List<Categoria>> getAllDescricao(@PathVariable String descricao) {
+
+		return ResponseEntity.ok(repository.findAllByDescricaoContainingIgnoreCase(descricao));
 	}
-	
-	
+
 	@PostMapping
-	public ResponseEntity<Categoria> post(@RequestBody Categoria categoria){
-		
-		return ResponseEntity.status(HttpStatus.CREATED)
-				.body(repository.save(categoria));
+	public ResponseEntity<Categoria> post(@RequestBody Categoria categoria) {
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(categoria));
 	}
-	
+
 	@PutMapping
-	public ResponseEntity<Categoria> put(@RequestBody Categoria categoria){
-		
-		return ResponseEntity.status(HttpStatus.CREATED)
-				.body(repository.save(categoria));
+	public ResponseEntity<Categoria> put(@RequestBody Categoria categoria) {
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(categoria));
 	}
-	
+
 	@DeleteMapping("/{id}")
-	public void delete(@PathVariable Long id){
+	public void delete(@PathVariable Long id) {
 		repository.deleteById(id);
 	}
-	
-	
-	
-	
-	
-	
+
 }
